ca457d04bf56ae267b8d0384922db4e8ad79d439
Aniket <aniketkumarg99@gmail.com>
2026-04-04 23:17:55 +0530

    margin top fix in all pages (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-screen flex flex-col bg-[#fcf8f8] text-[#1c1b1b] font-body">
         <Navbar />
-        <main className="flex-1 max-w-[1440px] mx-auto w-full pt-16">{children}</main>
+        <main className="flex-1 max-w-[1440px] mx-auto w-full">{children}</main>
         <Footer />
         <CookieBanner />
       </body>
